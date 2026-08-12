53b23b5f27055e36bb01047b748ebc99434f9eb6
amanimavu <amanimavu@gmail.com>
2026-08-12 14:49:14 +0300

    feat: add font configuration for Montserrat in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+    adapter: cloudflare(),
+    fonts: [
+        {
+            provider: fontProviders.fontsource(),
+            name: "Montserrat",
+            cssVariable: "--font-montserrat",
+        },
+    ],
 });
