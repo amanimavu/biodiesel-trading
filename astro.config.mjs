a3cdfde89aba7486d2915f48ed0706f8b9ebacf5
amanimavu <amanimavu@gmail.com>
2026-09-03 15:16:44 +0300

    chore: improve site SEO
    - Add meta description
    - Add open graph / twitter card tags
    - Integrate @astro/sitemap to help with sitemap generation
    - Add robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,28 @@ import { defineConfig, fontProviders } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://biodiesel.goldentie.africa",
-    adapter: cloudflare({
-        imageService: "compile",
-    }),
-    fonts: [
-        {
-            provider: fontProviders.fontsource(),
-            name: "Montserrat",
-            cssVariable: "--font-montserrat",
-        },
-    ],
-    image: {
-        layout: "constrained",
-        responsiveStyles: true,
-    },
+  site: "https://biodiesel.goldentie.africa",
+
+  adapter: cloudflare({
+      imageService: "compile",
+  }),
+
+  fonts: [
+      {
+          provider: fontProviders.fontsource(),
+          name: "Montserrat",
+          cssVariable: "--font-montserrat",
+      },
+  ],
+
+  image: {
+      layout: "constrained",
+      responsiveStyles: true,
+  },
+
+  integrations: [sitemap()],
 });
